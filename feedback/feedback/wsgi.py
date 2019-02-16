3ae85b92387fa19b964040c830249e19feb4185f
vahalova <vahalova@gmail.com>
2019-02-16 12:34:40 +0100

    Try fix wsgi

--- feedback/feedback/wsgi.py
+++ feedback/feedback/wsgi.py
@@ -17,7 +17,7 @@ https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE",
-    "feedback.settings")
+    "mysite.settings")
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
